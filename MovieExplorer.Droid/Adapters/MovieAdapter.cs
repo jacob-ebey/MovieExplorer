04903fe69d7b646b862d7975908890139cd3468b
Jacob Ebey <jacob.ebey@live.com>
2017-03-17 02:41:03 -0700

    - Updated nuget packages

--- MovieExplorer.Droid/Adapters/MovieAdapter.cs
+++ MovieExplorer.Droid/Adapters/MovieAdapter.cs
@@ -4,7 +4,6 @@ using MovieExplorer.Views;
 using MovieExplorer.Models;
 using System.Collections.ObjectModel;
 using System.Net.Http;
-using ModernHttpClient;
 using System.Threading.Tasks;
 using Android.Graphics.Drawables;
 using Android.Widget;
